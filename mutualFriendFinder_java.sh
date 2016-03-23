c5ccd774458b619b5f2b5bee8ce7e4292d7af9a9
Colby Snedeker <Woden501@gmail.com>
2016-03-23 16:04:32 -0400

    Updated hadoop run script.

--- mutualFriendFinder_java.sh
+++ mutualFriendFinder_java.sh
@@ -1,9 +1,9 @@
-hadoop fs -mkdir colbySnedekerProject2/input
-hadoop fs -put sample_input.csv colbySnedekerProject2/input
+hadoop fs -mkdir colbySnedekerProject3/input
+hadoop fs -put sample.txt colbySnedekerProject3/input
 
-mkdir matrixMultiplication_class
-javac -classpath /opt/hadoop/hadoop-core-1.2.1.jar:/lib/commons-cli-1.2.jar -d matrixMultiplication_class MatrixMultiplication.java
-jar -cvf MatrixMultiplication.jar -C /home/vcslstudent/colbySnedekerProject2/matrixMultiplication_class/ .
-hadoop jar /home/vcslstudent/colbySnedekerProject2/MatrixMultiplication.jar snedeker.cc.project2.MatrixMultiplication 10 5 /user/vcslstudent/colbySnedekerProject2/input/sample_input.csv /user/vcslstudent/colbySnedekerProject2/MatrixMultiplication_out_java
+mkdir mutualFriendFinder_class
+javac -classpath /opt/hadoop/hadoop-core-1.2.1.jar -d mutualFriendFinder_class MutualFriendFinder.java
+jar -cvf MutualFriendFinder.jar -C /home/vcslstudent/colbySnedekerProject3/mutualFriendFinder_class/ .
+hadoop jar /home/vcslstudent/colbySnedekerProject3/MutualFriendFinder.jar snedeker.cc.project3.MutualFriendFinder /user/vcslstudent/colbySnedekerProject3/input/sample.txt /user/vcslstudent/colbySnedekerProject3/MutualFriendFinder_out_java
 
-hadoop fs -cat /user/vcslstudent/colbySnedekerProject2/MatrixMultiplication_out_java/part-r-00000
+hadoop fs -cat /user/vcslstudent/colbySnedekerProject3/MutualFriendFinder_out_java/part-r-00000
